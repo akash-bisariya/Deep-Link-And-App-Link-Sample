a802260022c348ab2d6476ebead0764627639267
Akash Bisariya <akash.bisariya@affle.com>
2017-12-28 19:09:28 +0530

    initial commit

---
+++ app/src/main/java/app/android/com/deeplinksample/MainActivity.java
@@ -0,0 +1,21 @@
+package app.android.com.deeplinksample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("new intent","new intent");
+    }
+}
