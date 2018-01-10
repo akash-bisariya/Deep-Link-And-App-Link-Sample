f5f9565dad137180cfee7fca76b65104f81301fc
Akash Bisariya <akash.bisariya@affle.com>
2018-01-10 13:48:03 +0530

    Deeplink asset-statement added in strings.xml file

--- app/src/main/java/app/android/com/deeplinksample/MainActivity.java
+++ app/src/main/java/app/android/com/deeplinksample/MainActivity.java
@@ -1,21 +1,37 @@
 package app.android.com.deeplinksample;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.view.CameraView;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         Log.d("new intent","new intent");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
